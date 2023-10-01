b5ff166d24b1916f97bfdb60cf4a2b3f139645a7
Lyton <65817176+Lyton505@users.noreply.github.com>
2023-09-30 19:49:36 -0500

    added comments and btn selector

--- etch_sketch/main-script.js
+++ etch_sketch/main-script.js
@@ -1,3 +1,4 @@
+// this function creates the grid
 const addElements = () => {
     for (let j = 0; j < 16; j++) {
         let rowDiv = document.createElement('div')  //new div to put box div. each a flex item
@@ -10,61 +11,50 @@ const addElements = () => {
             let col1 = 0;
             let col2 = 0;
             let col3 = 0;
-            [col1, col2, col3] = color();
+            [col1, col2, col3] = color(); // destructuring
             thisDiv.setAttribute('style', `
                 background: rgba(${col1},${col3}, ${col2}, 0.95 );
                 color:black;
                 border: 2px solid black;
             `);
-            // thisDiv.innerText = `${i + 1}`;
-
-            rowDiv.appendChild(thisDiv);
+            rowDiv.appendChild(thisDiv); //append each loop div to rowdiv
         }
-        divContainer.appendChild(rowDiv)
+        divContainer.appendChild(rowDiv) //append final rowDiv to the divContainer
     }
-    // console.log(divContainer);
 }
-const gridContainer = document.querySelectorAll('.squareGrid');
-const divContainer = gridContainer[0];
 
+// this function returns a list of 3 values(0-255); can be used to randomize the colors
 const color = () => {
     let colorDiv = [];
 
-
     for (let j = 0; j <= 2; j++) {
         // set to white
-        let colorValue = 120;
-
-        // set to random color
-        // let colorValue = Math.floor(Math.random() * 240);
-        // while (colorValue < 120){
-        //     ++colorValue
-        // }
+        let colorValue = 255;
 
         colorDiv.push(colorValue);
     }
     return colorDiv;
 }
 
+// this function changes the background color of the element that has been passed in
 const changeColor = (elementIn) => {
-    // set to random color
-    // let colorValue = Math.floor(Math.random() * 240);
-    // while (colorValue < 120){
-    //     ++colorValue
-    // console.log(elementIn)
     elementIn.style.backgroundColor = 'blue';
-    
+
 }
 
+// variable initialization
+const gridContainer = document.querySelectorAll('.squareGrid');
+const divContainer = gridContainer[0];
 
 addElements()
-
-
+// order matters. boxDiv can only be declared after addElements
 const boxDiv = document.querySelectorAll('.box');
 
-
+// eventListener on each button
 boxDiv.forEach((myElement) => {
     myElement.addEventListener("mouseover", () => {
         changeColor(myElement)
     })
 });
+
+const btnGrid = document.querySelector('#buttonGrid')
