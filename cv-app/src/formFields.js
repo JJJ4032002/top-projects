626369ae5798848942c812140df72e853fc1de1d
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-03-03 16:32:47 -0600

    added separated field info

---
+++ cv-app/src/formFields.js
@@ -0,0 +1,11 @@
+const fields = [
+    { name: "firstName", label: "First Name", type: "text" },
+    { name: "lastName", label: "Last Name", type: "text" },
+    { name: "myLink", label: "Relevant web link", type: "text" },
+    { name: "myEmail", label: "Email", type: "email" },
+    {name: "mySchool", label: "School", type: "text"},
+    {name: "myDegree", label: "Degree Type", type: "text"},
+    {name: "myGradDate", label: "Graduation date", type: "date"},
+];
+
+export default fields;
