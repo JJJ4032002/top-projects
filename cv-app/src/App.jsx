4f89aa68b4dca599cf8c27cd605f0d870bde99fd
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-03-03 16:13:58 -0600

    added header and education forms

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -7,7 +7,11 @@ function App() {
     const fields = [
         { name: "firstName", label: "First Name", type: "text" },
         { name: "lastName", label: "Last Name", type: "text" },
+        { name: "myLink", label: "Relevant web link", type: "text" },
         { name: "myEmail", label: "Email", type: "email" },
+        {name: "mySchool", label: "School", type: "text"},
+        {name: "myDegree", label: "Degree Type", type: "text"},
+        {name: "myGradDate", label: "Graduation date", type: "date"},
     ];
 
     const handleSubmit = (formData) => {
@@ -15,10 +19,10 @@ function App() {
     };
 
     return (
-        <>
-            <Header fields={fields.slice(0,2)} onSubmit={handleSubmit}></Header>
-            {/*<GenericForm fields={fields} onSubmit={handleSubmit}></GenericForm>*/}
-        </>
+        <div>
+            <Header fields={fields.slice(0,4)} onSubmit={handleSubmit}></Header>
+            <GenericForm fields={fields.slice(4)} onSubmit={handleSubmit}></GenericForm>
+        </div>
     );
 }
 
