a561c0be233dbaef0216fcbab46d523fa2aabf31
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-03-03 16:41:44 -0600

    prettified

--- cv-app/src/formFields.js
+++ cv-app/src/formFields.js
@@ -3,9 +3,14 @@ const fields = [
     { name: "lastName", label: "Last Name", type: "text" },
     { name: "myLink", label: "Relevant web link", type: "text" },
     { name: "myEmail", label: "Email", type: "email" },
-    {name: "mySchool", label: "School", type: "text"},
-    {name: "myDegree", label: "Degree Type", type: "text"},
-    {name: "myGradDate", label: "Graduation date", type: "date"},
+    { name: "mySchool", label: "School", type: "text" },
+    { name: "myDegree", label: "Degree Type", type: "text" },
+    { name: "myGradDate", label: "Graduation date", type: "date" },
+    { name: "myLastEmployer", label: "Employer", type: "text" },
+    { name: "myLastTitle", label: "Role/Title", type: "text" },
+    { name: "myLastStartDate", label: "Start date", type: "date" },
+    { name: "myLastEndDate", label: "End date", type: "date" },
+    { name: "myLastDescription", label: "Description", type: "text" },
 ];
 
 export default fields;
