1f7fe9075fed0f3e24989864cc918cb14d63beeb
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-03-03 15:14:03 -0600

    prettified

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import GenericForm from "./components/GenericForm/GenericForm.jsx";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
-
     const fields = [
-        {name: 'firstName', label:'First Name', type: 'text'},
-        {name: 'lastName', label: 'Last Name', type: 'text'},
-        {name: 'myEmail', label: 'Email', type: 'email'},
-    ]
+        { name: "firstName", label: "First Name", type: "text" },
+        { name: "lastName", label: "Last Name", type: "text" },
+        { name: "myEmail", label: "Email", type: "email" },
+    ];
 
     const handleSubmit = (formData) => {
-        console.log('Form data: ', formData);
-    }
+        console.log("Form data: ", formData);
+    };
 
-  return (
-   <>
-   <GenericForm fields={fields} onSubmit={handleSubmit}></GenericForm>
-   </>
-  )
+    return (
+        <>
+            <Header fields={fields.slice(0,2)} onSubmit={handleSubmit}></Header>
+            {/*<GenericForm fields={fields} onSubmit={handleSubmit}></GenericForm>*/}
+        </>
+    );
 }
 
-export default App
+export default App;
