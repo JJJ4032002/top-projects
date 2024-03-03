f6f86db8e2fa4ae1f33218aff6e7bd0dd7625096
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-03-03 16:51:24 -0600

    added all other sections

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -5,16 +5,28 @@ import Header from "./components/Header/Header.jsx";
 import fields from "./formFields.js";
 
 function App() {
-
     const handleSubmit = (formData) => {
         console.log("Form data: ", formData);
     };
 
     return (
-        <div>
-            <Header fields={fields.slice(0,4)} onSubmit={handleSubmit} ></Header>
-            <GenericForm fields={fields.slice(4)} onSubmit={handleSubmit} title={'Education'}></GenericForm>
-        </div>
+        <>
+            <h2>Resume Builder</h2>
+            <div className={'formDiv'}>
+                <Header fields={fields.slice(0, 4)} onSubmit={handleSubmit}></Header>
+                <GenericForm fields={fields.slice(4, 7)} onSubmit={handleSubmit} title={"Education"}></GenericForm>
+                <GenericForm
+                    fields={fields.slice(7, 12)}
+                    onSubmit={handleSubmit}
+                    title={"Most Recent Work XP"}
+                ></GenericForm>
+                <GenericForm
+                    fields={fields.slice(12)}
+                    onSubmit={handleSubmit}
+                    title={"Most Recent Work XP"}
+                ></GenericForm>
+            </div>
+        </>
     );
 }
 
