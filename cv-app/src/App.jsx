74c5b09be02ffcd02e322ae48557aced2296720a
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-03-03 16:33:13 -0600

    separated fields logic

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -2,17 +2,9 @@ import { useState } from "react";
 import "./App.css";
 import GenericForm from "./components/GenericForm/GenericForm.jsx";
 import Header from "./components/Header/Header.jsx";
+import fields from "./formFields.js";
 
 function App() {
-    const fields = [
-        { name: "firstName", label: "First Name", type: "text" },
-        { name: "lastName", label: "Last Name", type: "text" },
-        { name: "myLink", label: "Relevant web link", type: "text" },
-        { name: "myEmail", label: "Email", type: "email" },
-        {name: "mySchool", label: "School", type: "text"},
-        {name: "myDegree", label: "Degree Type", type: "text"},
-        {name: "myGradDate", label: "Graduation date", type: "date"},
-    ];
 
     const handleSubmit = (formData) => {
         console.log("Form data: ", formData);
@@ -20,8 +12,8 @@ function App() {
 
     return (
         <div>
-            <Header fields={fields.slice(0,4)} onSubmit={handleSubmit}></Header>
-            <GenericForm fields={fields.slice(4)} onSubmit={handleSubmit}></GenericForm>
+            <Header fields={fields.slice(0,4)} onSubmit={handleSubmit} ></Header>
+            <GenericForm fields={fields.slice(4)} onSubmit={handleSubmit} title={'Education'}></GenericForm>
         </div>
     );
 }
