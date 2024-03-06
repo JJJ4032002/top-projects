462ff1aceae6b088581ea7191a4938e45db91f52
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-03-05 20:32:40 -0600

    added code to get list of heroes

---
+++ memory-card/src/Heroes.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import CryptJS from "crypto-js";
+
+function hashingAlgo(pub, priv, currTime) {
+    return CryptJS.MD5(currTime + priv + pub).toString();
+}
+
+const getHeroes = async (url, oldHeroes) => {
+    try {
+        const response = await axios.get(url);
+        // console.log("Successful:", response.data.data.results);
+        const newHeroes = response.data.data.results;
+        const heroList = [...oldHeroes, ...newHeroes];
+        // console.log("Heroes from api:", newHeroes);
+        return heroList;
+    } catch (error) {
+        // console.log("Failed:", error);
+        return oldHeroes;
+    }
+};
+
+let heroLibrary = [{ name: "lyton" }];
+const publicKeyCredential = import.meta.env.VITE_REACT_APP_MARVEL_API_KEY_PUBLIC;
+const privateKeyCredential = import.meta.env.VITE_REACT_APP_MARVEL_API_KEY_PRIVATE;
+const currTime = new Date().getTime();
+const hashedUrl =
+    `https://gateway.marvel.com/v1/public/characters?ts=${currTime}&apikey=${publicKeyCredential}` +
+    `&hash=${hashingAlgo(publicKeyCredential, privateKeyCredential, currTime)}`;
+
+const updateLibrary = async function () {
+    heroLibrary = await getHeroes(hashedUrl, heroLibrary);
+};
+
+updateLibrary();
+
+export const getHeroLibrary = () => {return heroLibrary || []};
