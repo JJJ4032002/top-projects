d5ca7c7c9709cc9dfc28d98afba6871eb6660205
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-03-06 21:10:51 -0600

    conditional rendering for loading

--- memory-card/src/components/HeroCard.jsx
+++ memory-card/src/components/HeroCard.jsx
@@ -1,12 +1,22 @@
-function HeroCard(heroes) {
+function HeroCard({heroes, loadingState}) {
 
-    console.log("ohh, heroes: ", heroes)
+    console.log("ohh, heroes: ", heroes[0])
+    console.log("Current loading state: ", loadingState)
 
     return (
         <>
+            {loadingState ?
+                <div>
+                    ...assembling the avengers
+                </div> :
+                (Array.isArray(heroes) && heroes.length > 1) ?
+                    <h2>{"Num 1 is " + heroes[0].name}</h2> :
+                    <p>Avengers are busy at the moment :-/</p>
+            }
+
             {/*<h2>{hero.name}</h2>*/}
             {/*<img src={heroImageUrl} alt={"An image of" + heroName}/>*/}
-            { heroes !== null && <h2>{"Num 1 is " + heroes}</h2>}
+
         </>
     )
 }
