d8600f27ef91ae4b8ef7cba79b56a32faa224c6b
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-03-07 00:30:35 -0600

    added logic for prefiltering heroes

--- memory-card/src/components/HeroCard.jsx
+++ memory-card/src/components/HeroCard.jsx
@@ -2,43 +2,49 @@ import './HeroCard.css'
 
 function HeroCard({heroes, loadingState}) {
 
+    const filteredHeroes = heroes.filter(
+        hero => hero.thumbnail.path !== "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
+    );
+
+    console.log("Filtered heroes: ", filteredHeroes)
+
     const renderContent = () => {
         if (loadingState) {
             return <div>...assembling the avengers</div>;
         }
-
-        if (Array.isArray(heroes) && heroes.length > 0) {
+        if (Array.isArray(filteredHeroes) && filteredHeroes.length > 0) {
             return (
-                heroes.map((hero, index) => {
-                    return (
-                        <div className={'heroCardDiv'} key={hero.id}>
-                            <h2>{hero.name}</h2>
-                            <img className={'heroImage'}
-                                 src={hero.thumbnail.path + "/landscape_xlarge." + hero.thumbnail.extension}
-                                 alt={"An image of the hero, " + hero.name}
-                            />
-                        </div>
-                    )
-                })
-
-            );
+                <>
+                    {filteredHeroes.map((hero) => {
+                        return (
+                            <div className={'heroCardDiv'} key={hero.id}>
+                                <h2>{hero.name}</h2>
+                                <img className={'heroImage'}
+                                     src={hero.thumbnail.path + "/landscape_xlarge." + hero.thumbnail.extension}
+                                     alt={"An image of the hero, " + hero.name}
+                                />
+                            </div>
+                        )
+                    })}
+                </>);
         }
 
         return <p>Avengers are busy at the moment :-/</p>;
     }
 
-    console.log("ohh, heroes: ", heroes[0])
+    console.log("ohh, my first hero: ", heroes[0])
+    console.log("Current heroes: ", heroes)
     console.log("Current loading state: ", loadingState)
 
-    return (<>
+    return (
+
         <>
+            {filteredHeroes.length > 0 && <h2 className={'heroCount'}>Showing {filteredHeroes.length} heroes</h2>}
             <div className={'heroCardsContainer'}>
                 {renderContent()}
             </div>
-
         </>
-
-    </>)
+    )
 }
 
 export default HeroCard;
