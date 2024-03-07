3c4e0c702403e2b722e568408997996d07aaced8
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-03-06 21:39:12 -0600

    moved conditional rendering logic to function to make it more readable

--- memory-card/src/components/HeroCard.jsx
+++ memory-card/src/components/HeroCard.jsx
@@ -1,24 +1,33 @@
 function HeroCard({heroes, loadingState}) {
 
+    const renderContent = () => {
+        if (loadingState) {
+            return <div>...assembling the avengers</div>;
+        }
+
+        if (Array.isArray(heroes) && heroes.length > 0) {
+            return (
+                <div>
+                    <h2>{heroes[0].name}</h2>
+                    <img src={heroes[0].thumbnail.path+"/landscape_xlarge."+heroes[0].thumbnail.extension}
+                         alt={"An image of" + heroes[0].name}/>
+                </div>
+
+            );
+        }
+
+        return <p>Avengers are busy at the moment :-/</p>;
+    }
+
     console.log("ohh, heroes: ", heroes[0])
     console.log("Current loading state: ", loadingState)
 
-    return (
+    return (<>
         <>
-            {loadingState ?
-                <div>
-                    ...assembling the avengers
-                </div> :
-                (Array.isArray(heroes) && heroes.length > 1) ?
-                    <h2>{"Num 1 is " + heroes[0].name}</h2> :
-                    <p>Avengers are busy at the moment :-/</p>
-            }
-
-            {/*<h2>{hero.name}</h2>*/}
-            {/*<img src={heroImageUrl} alt={"An image of" + heroName}/>*/}
-
+            {renderContent()}
         </>
-    )
+
+    </>)
 }
 
 export default HeroCard;
