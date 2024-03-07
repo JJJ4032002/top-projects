3f3f7d156db19999939ddaa0ad40761b1ccd7f39
Lyton Mhlanga <65817176+Lyton505@users.noreply.github.com>
2024-03-06 21:10:37 -0600

    added loading status

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -4,13 +4,16 @@ import {heroesEndpoint} from "./Heroes.js";
 import HeroCard from "./components/HeroCard.jsx";
 
 function App() {
+    const [isLoading, setLoading] = useState(true);
     const [heroes, setHeroes] = useState([]);
 
     useEffect( () => {
         const fetchHeroes = async () => {
+            setLoading(true);
             const heroLibrary = await heroesEndpoint();
             console.log("Main app heroes: ", heroLibrary);
             setHeroes([...heroes, ...heroLibrary]);
+            setLoading(false)
         };
 
         fetchHeroes();
@@ -19,7 +22,7 @@ function App() {
     return (
         <>
             <h2>Galaxy Heroes</h2>
-            <HeroCard heroesApi={ heroes }></HeroCard>
+            <HeroCard heroes={ heroes } loadingState={isLoading}></HeroCard>
 
             {/*<ul>*/}
             {/*    {heroes.map((hero, index) => {*/}
